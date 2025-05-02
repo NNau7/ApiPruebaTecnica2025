cf9ef813d22a6e75a8b09abf6dcf244d1bdbe584
Mauri Ribotta <mauriribotta2@gmail.com>
2025-05-02 20:08:19 -0300

    Program.cs

--- ApiPruebaTecnicav2/Program.cs
+++ ApiPruebaTecnicav2/Program.cs
@@ -6,10 +6,10 @@ using Microsoft.OpenApi.Models;
 
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
+
 
 builder.Services.AddControllers();
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 builder.Services.AddDbContext<ApiDbContext>(options => options.UseSqlServer(builder.Configuration.GetConnectionString("ApiConnection")));
@@ -24,7 +24,6 @@ var app = builder.Build();
 //}
 
 
-// Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
